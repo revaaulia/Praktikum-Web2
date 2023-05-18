84a6dc9d2946dc1ae12d3c3166473e9df6248463
revaaulia <revaauliafaradilah@gmail.com>
2023-05-18 13:45:33 +0700

    praktikum & tugas week8 - laravel

---
+++ laravel/olshop/resources/views/profilenf.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Profile STTNF</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  </head>
+  <body>
+    <h3>Sekolah Tinggi Teknologi Terpadu Nurul Fikri</h1>
+    <br>
+    <h4>Lokasi Kampus A : Jl. Situ Indah no.116, Cimanggis, Depok <br>
+    Kampus B: Jl. Raya Lenteng Agung No.20, Jagakarsa </h4>
+    <br>
+    <h4>Telepon: 021 - 786 3191</h4>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ laravel/olshop/routes/web.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/salam', function () {
+    return "Halo Selamat Sore Teman-teman Semuaa..";
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "tittle" => "Halaman Profile",
+        "nama" => "Reva Aulia Faradilah",
+        "umur" => 20,
+        "email" => "revaauliafaradilah@gmail.com",
+    ]);
+});
+
+Route::get('/profilenf', function () {
+    return view('profilenf');
+});
+
+Route::get('/pemeriksaan', function () {
+    return view('pemeriksaan', [
+        "title" => "Form Pemeriksaan",
+        "tekanandarah" => "120/80 mmhg",
+        "asamurat" => "Pria : < 7 mg/dl | Wanita: < 6 mg/dl",
+        "kolesterol" => "< 200 mg/dl",
+        "guladarah1" => "Puasa : 70-110 mg/dl",
+        "guladarah2" => "2 jam setelah makan : 100-150 mg/dl",
+        "guladarah3" => "Sewaktu/acak : 70-125 mg/dl",
+    ]);
+});
